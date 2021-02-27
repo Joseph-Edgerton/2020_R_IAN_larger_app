0101d1da77a4f4fcc9570c8b5613c0d3714d06c9
Joseph Edgerton <joseph.h.edgerton@gmail.com>
2021-02-26 22:47:18 -0800

    Figuring out tab set up

--- IAN_newsletter_shiny_app.R
+++ IAN_newsletter_shiny_app.R
@@ -1,8 +1,29 @@
 
+
+library(tidyverse)
+library(GGally)
+library(network)
+library(here)
+library(ggimage)
+library(googlesheets4)
+library(sna)
+library(gt)
 library(shiny)
+library(lubridate)
+library(shinyscreenshot)
+library(gcalendr)
+library(scales)
 
 # Define UI
-ui <- fluidPage()
+ui <- fluidPage(
+    mainPanel(
+        tabsetPanel(
+            tabPanel("Plot", plotOutput("plot")),
+            tabPanel("Summary", verbatimTextOutput("summary")),
+            tabPanel("Table", tableOutput("table"))
+        )
+    )
+)
 
 # Define server logic 
 server <- function(input, output) {
@@ -11,3 +32,11 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
+
+strftime(seq(as.POSIXct('00:00', format = "%H:%M", tz = "UTC"), 
+           as.POSIXct(Sys.Date() + 1), by = '30 min'), format = "%I:%M %p", tz = "EST", usetz = TRUE)
+
+
